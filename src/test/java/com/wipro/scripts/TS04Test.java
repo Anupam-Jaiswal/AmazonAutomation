47e63c6f5ba1d852298f60ea5f35d45e0b0022f6
Änupam-Jaiswal <jaiswal.anupam2013@gmail.com>
2021-06-14 23:56:51 +0530

    email notification check commit 1

--- src/test/java/com/wipro/scripts/TS04Test.java
+++ src/test/java/com/wipro/scripts/TS04Test.java
@@ -15,6 +15,6 @@ public class TS04Test extends StartBrowser
 		cfs.login();
 		cfs.addNewAddress();
 		cfs.removeAddress();
-		//email notification check commit
+		//email notification checkin commit1
 	}
 }
