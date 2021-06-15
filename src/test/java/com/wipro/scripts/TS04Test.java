7d71a4d72f2ccea534ee7c22d27b54b41e72b03d
Änupam-Jaiswal <jaiswal.anupam2013@gmail.com>
2021-06-15 22:15:01 +0530

    final email extension commit

--- src/test/java/com/wipro/scripts/TS04Test.java
+++ src/test/java/com/wipro/scripts/TS04Test.java
@@ -15,6 +15,6 @@ public class TS04Test extends StartBrowser
 		cfs.login();
 		cfs.addNewAddress();
 		cfs.removeAddress();
-		//final  yahoo email checking
+		//final email extension checking
 	}
 }
